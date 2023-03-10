32e447b1e22c0e37c39b8fe4a36ef2dd87fc8064
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2023-03-10 05:35:30 +0200

    Add project files.

---
+++ controlsController/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace controlsController
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public controlscontrollerclass cc = new controlscontrollerclass();
+
+        public UserControl uc = new UserControl();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cc.addControlInListOfControls(new Button());
+            cc.setPositionOfControl(0, 10, 10);
+            cc.setTextOfControl(0, "OK");
+            cc.addControlToForm(this, 0);
+            cc.setVisibilityOfControl(0, true);
+
+            uc.Width = 500;
+            uc.Height = 500;
+            uc.Left = 200;
+            uc.Top = 200;
+            uc.BackColor = Color.White;
+            uc.Visible = true;
+            this.Controls.Add(uc);
+            uc.Controls.Add(cc.controlsControllerList[0]);
+        }
+    }
+}
---
+++ controlsController/controlscontrollerclass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace controlsController
+{
+    public class controlscontrollerclass
+    {
+        public List<Control> controlsControllerList = new List<Control>();
+        public controlscontrollerclass() { }
+        public controlscontrollerclass(Control control) { 
+            this.controlsControllerList.Add(control);
+        }
+        public void addControlInListOfControls(Control control)
+        {
+            this.controlsControllerList.Add(control);
+        }
+        public void setTextOfControl(int index, string s)
+        {
+            this.controlsControllerList[index].Text = s;
+        }
+        public void setVisibilityOfControl(int index, bool yn)
+        {
+            this.controlsControllerList[index].Visible = yn;
+        }
+        public void setPositionOfControl(int index, int x, int y)
+        {
+            this.controlsControllerList[index].Left = x;
+            this.controlsControllerList[index].Top = y;
+        }
+        public void addControlToForm(Form f, int index)
+        {
+            f.Controls.Add(controlsControllerList[index]);
+
+        }
+    }
+}
